948ba197584f6ee0608d1c8ddfbfbedc7353c7a4
Chris Malek <cmalek@caltech.edu>
2016-04-06 11:29:22 -0700

    updated to 0.5.2

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Chef'
 maintainer_email 'Sean OMeara <someara@chef.io>'
 license 'Apache 2.0'
 description 'Installs/Configures yum-repoforge'
-version '0.5.1'
+version '0.5.2'
 
 depends 'yum', '~> 3.0'
 depends 'yum-epel'
